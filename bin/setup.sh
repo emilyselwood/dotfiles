11ec7487e583c8467c3d0e1a1c1e6126f16c3261
Wil Selwood <wil.selwood@sa.catapult.org.uk>
2018-02-13 15:08:00 +0000

    dep download

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,11 @@ apt update
 
 apt upgrade
 
-apt install build-essential git ssh vim vim-enhanced tmux make
+apt install build-essential git ssh vim vim-enhanced tmux make wget
+
+wget https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64 -O ~/bin/dep
+
+chmod +x ~/bin/dep
 
 
 
