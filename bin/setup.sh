b72a73532374cdf1707178bf13777fa1850ba6e1
Wil Selwood <wil.selwood@sa.catapult.org.uk>
2018-02-14 12:33:50 +0000

    fix install script

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,11 +5,13 @@ apt update
 
 apt upgrade
 
-apt install build-essential git ssh vim vim-enhanced tmux make wget
+apt install build-essential git ssh vim vim-enhanced tmux make wget zip
 
 wget https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64 -O ~/bin/dep
-
 chmod +x ~/bin/dep
 
-
-
+wget https://dl.google.com/go/go1.9.4.linux-amd64.tar.gz -O ~/go-linux.tar.gz
+tar xzf ~/go-linux.tar.gz -C ~/
+ln -s ~/go/bin/go ~/bin/go
+ln -s ~/go/bin/godoc ~/bin/godoc
+ln -s ~/go/bin/gofmt ~/bin/gofmt
