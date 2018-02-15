f729344818fd7ac5427e56deaa1b75d230980d4a
Wil Selwood <wil.selwood@sa.catapult.org.uk>
2018-02-15 10:52:48 +0000

    add python3 and pip

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,10 +2,10 @@
 
 # This needs to be run with sudo
 apt update
-
 apt upgrade
+apt install build-essential git ssh vim vim-enhanced tmux make wget zip python3 python3-pip
 
-apt install build-essential git ssh vim vim-enhanced tmux make wget zip
+pip3 install --upgrade pip
 
 wget https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64 -O ~/bin/dep
 chmod +x ~/bin/dep
