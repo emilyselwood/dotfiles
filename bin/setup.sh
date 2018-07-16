9616aed974fbe8f64cb4e21c726734f783074fdc
Wil Selwood <wil.selwood@sa.catapult.org.uk>
2018-07-16 14:13:40 +0100

    update to make work again

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,10 +2,12 @@
 
 # This needs to be run with sudo
 apt update
-apt upgrade
-apt install build-essential git ssh vim vim-enhanced tmux make wget zip python3 python3-pip
+apt upgrade -y
+apt install -y build-essential git ssh vim tmux make wget zip python3 python3-pip
 
 pip3 install --upgrade pip
+
+mkdir -p ~/bin
 
 wget https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64 -O ~/bin/dep
 chmod +x ~/bin/dep
@@ -15,3 +17,8 @@ tar xzf ~/go-linux.tar.gz -C ~/
 ln -s ~/go/bin/go ~/bin/go
 ln -s ~/go/bin/godoc ~/bin/godoc
 ln -s ~/go/bin/gofmt ~/bin/gofmt
+
+cp ../.* ~/
+
+ln -s /mnt/c/git /git
+ln -s /mnt/c/data /data
